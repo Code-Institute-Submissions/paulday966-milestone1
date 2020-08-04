9011c9d597bd4d1942b6151de4a70b2eee7ccd5d
paulday966 <paulday360@outlook.com>
2020-08-04 21:17:06 +0000

    Copied the functions from the bottom of the contact us page and pasted them in here

---
+++ my-functions.js
@@ -0,0 +1,7 @@
+function NewsLetterSubmit() {
+  alert("Thank you for subscribing to our newsletter");
+}
+
+function CafeRequestSubmit() {
+  alert("Thank you for sending us details of the cafe");
+}
